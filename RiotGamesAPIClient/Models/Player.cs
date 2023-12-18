158db261b7fcb49d207afbdc445bcb6872668ce2
ranforrestren <ranforrestren@gmail.com>
2023-12-18 13:35:14 -0500

    Create Player Model class and Database Context

---
+++ RiotGamesAPIClient/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RiotGamesAPIClient.Models
+{
+    public class Player
+    {
+        [Key]
+        public String PlayerId { get; set; } // pUUID
+        [Required]
+        [MaxLength(50)]
+        public String PlayerName { get; set; } // gameName
+        [Required]
+        [MaxLength(10)]
+        public String PlayerTagline { get; set; } // tagLine
+    }
+}
---
+++ RiotGamesAPIClient/Models/PlayerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RiotGamesAPIClient.Models
+{
+    public class PlayerDbContext : DbContext
+    {
+        public PlayerDbContext (DbContextOptions<PlayerDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Player> Players { get; set; }
+    }
+}
