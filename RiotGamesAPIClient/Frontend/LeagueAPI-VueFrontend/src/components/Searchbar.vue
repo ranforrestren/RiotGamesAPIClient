21dfb13b0bd5f9d82294d3ba3bdace338d1d48ad
ranforrestren <ranforrestren@gmail.com>
2023-12-28 19:03:34 -0500

    Code searchbar component to make Fetch API request to ASP.NET endpoint

--- RiotGamesAPIClient/Frontend/LeagueAPI-VueFrontend/src/components/Searchbar.vue
+++ RiotGamesAPIClient/Frontend/LeagueAPI-VueFrontend/src/components/Searchbar.vue
@@ -1,18 +1,22 @@
 <script setup>
 import { ref } from 'vue';
+    let gameName = null;
+    let tagLine = null;
 
-defineProps({
-    gameName: String,
-    tagLine: String,
-});
+async function getByName(name, tag) {
+    const response = await fetch(`https://localhost:7179/api/Players/byName/${name}/${tag}`);
+    const player = await response.json();
+    console.log(player);
+}
 </script>
 
 <template>
-    <form class="hflex" method="GET" action="https://localhost:7179/api/Players/byName/">
-        <input class="gameName" type="text" placeholder="LAGAVULIN">
-        #<input class="tagLine" type="text" placeholder="10SIX">
-        <input class="submit" type="submit">
-    </form>
+    <div class="hflex" method="GET" action="https://localhost:7179/api/Players/byName/">
+        <input class="gameName" type="text" placeholder="GAMENAME" v-model="gameName">
+        <div class="spacing">#</div>
+        <input class="tagLine" type="text" placeholder="TAGLINE" v-model="tagLine">
+        <input class="submit" type="submit" @click="getByName(gameName, tagLine)">
+    </div>
 </template>
 
 <style scoped>
@@ -22,16 +26,26 @@ defineProps({
         border-radius: 100px;
         align-items: center;
         color: #171717a0;
+        transition: filter 300ms;
     }
     input {
         border: none;
         background-color: transparent;
+        border: 1px solid transparent;
         font-size: 1rem;
         padding: 14px;
         color: #171717a0;
+        transition: border-color 300ms;
+    }
+    input:focus {
+        outline: none;
+    }
+    input:hover {
+        border-color: #a3a3a3a0;
     }
     .gameName {
         flex-grow: 4;
+        border-radius: 100px 0px 0px 100px;
     }
     .tagLine {
         flex-grow: 1;
@@ -39,6 +53,13 @@ defineProps({
     .submit {
         flex-grow: 2;
         background-color: #a3a3a360;
+        color: #171717a0;
         border-radius: 0px 100px 100px 0px;
+    }
+    .spacing {
+        padding: 10px;
+    }
+    .hflex:hover {
+        filter: drop-shadow(0 0 0.5em #40404040);
     }
 </style>
